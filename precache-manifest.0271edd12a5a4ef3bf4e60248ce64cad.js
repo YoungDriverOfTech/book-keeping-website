aa9b8852923ffb14bf8587f7f7bb7988abacb549
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-06 18:05:58 +0900

    deploy

---
+++ precache-manifest.0271edd12a5a4ef3bf4e60248ce64cad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ce2521d58f02a3c0cf",
+    "url": "/book-keeping-website/css/app.19abecbb.css"
+  },
+  {
+    "revision": "3a31b83baa2743a5053fca96a5df0ed2",
+    "url": "/book-keeping-website/index.html"
+  },
+  {
+    "revision": "a8ce2521d58f02a3c0cf",
+    "url": "/book-keeping-website/js/app.8a39313a.js"
+  },
+  {
+    "revision": "b244161a472aca78e4a7",
+    "url": "/book-keeping-website/js/chunk-vendors.4842461f.js"
+  },
+  {
+    "revision": "9687f909ab6db5f1b756e536556c3b17",
+    "url": "/book-keeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/book-keeping-website/robots.txt"
+  }
+]);
